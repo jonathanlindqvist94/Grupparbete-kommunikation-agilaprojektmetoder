c2dba92f66ff28973a990d99ec38e9f9673c1bb7
jonathanlindqvist94 <jonathanlindqvist94@gmail.com>
2021-10-19 08:48:08 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const button = document.querySelector('.btn-main');
+
+// button.addEventListener('click', checkPassword = (event) => {
+//     const password = document.querySelector('.password').value;
+//     const confirmPassword = document.querySelector('.confirm-password').value;
+//     const msg = document.querySelector('.message');
+
+
+//     if(password.length != 0) {
+//         if(password === confirmPassword) {
+//             msg.textContent = "passwords Match!";
+//             msg.style.backgroundColor = "#3ae374";
+//             msg.style.padding = ".5em 1.5em";
+//         } else {
+//             msg.textContent = "Passwords don't match!";
+//             msg.style.backgroundColor = "#ff4d4d";
+//             msg.style.padding = ".5em 1.5em";
+//             event.preventDefault();
+//         }
+//     } else {
+//         alert("Password can't be empty");
+//         msg.textContent = "";
+//         msg.style.padding = "0";
+
+//         event.preventDefault();
+//     }
+// });
+
+button.addEventListener('click', checkPassword = (event) => {
+    const password = document.querySelector('.password').value;
+    const confirmPassword = document.querySelector('.confirm-password').value;
+    const msg = document.querySelector('.message');
+
+    if (!password.length) {
+        alert("Password can't be empty");
+        msg.textContent = "";
+        msg.style.padding = "0";
+        event.preventDefault();
+        return;
+    }
+
+    if (password != confirmPassword) {
+        msg.textContent = "Passwords don't match!";
+        msg.style.backgroundColor = "#ff4d4d";
+        msg.style.padding = ".5em 1.5em";
+        event.preventDefault();
+        return;
+    }
+
+    msg.textContent = "passwords Match!";
+    msg.style.backgroundColor = "#3ae374";
+    msg.style.padding = ".5em 1.5em";
+});
